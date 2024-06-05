0ed01792fb62756cbd4bb5273a5baf148f446914
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-06-04 23:36:22 -0400

    updating

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-var widths = [];
-var lengths = [];
+const widths = [];
+const lengths = [];
 
 function sumarAncho() {
-    var width = parseFloat(document.getElementById("widthInput").value);
+    const width = parseFloat(document.getElementById("widthInput").value);
     widths.push(width);
     actualizarMedidas();
 }
 
 function sumarLargo() {
-    var length = parseFloat(document.getElementById("lengthInput").value);
+    const length = parseFloat(document.getElementById("lengthInput").value);
     lengths.push(length);
     actualizarMedidas();
 }
 
 function actualizarMedidas() {
-    var widthSum = widths.reduce((a, b) => a + b, 0);
-    var lengthSum = lengths.reduce((a, b) => a + b, 0);
-    var area = widthSum * lengthSum;
+    const widthSum = widths.reduce((a, b) => a + b, 0);
+    const lengthSum = lengths.reduce((a, b) => a + b, 0);
+    const area = widthSum * lengthSum;
     document.getElementById("result").innerText = "Medidas Generales del Terreno:\nAncho: " + widthSum.toFixed(2) + " metros\nLargo: " + lengthSum.toFixed(2) + " metros\nÁrea Total del Terreno: " + area.toFixed(2) + " metros cuadrados";
 }
 
 function calcularResultado() {
-    var tileSizeWidth = parseFloat(document.getElementById("tileSizeWidth").value);
-    var tileSizeLength = parseFloat(document.getElementById("tileSizeLength").value);
-    var tileSizeWidth = parseFloat(document.getElementById("tileSizeWidth").value);
-    var tileSizeLength = parseFloat(document.getElementById("tileSizeLength").value);
-    var tileSizeArea = tileSizeWidth * tileSizeLength;
+    const tileSizeWidth = parseFloat(document.getElementById("tileSizeWidth").value);
+    const tileSizeLength = parseFloat(document.getElementById("tileSizeLength").value);
+    const tileSizeArea = tileSizeWidth * tileSizeLength;
 
-    var area = widths.reduce((a, b) => a + b, 0) * lengths.reduce((a, b) => a + b, 0);
-    var palmetas = Math.ceil(area / tileSizeArea);
+    const terrainArea = parseFloat(document.getElementById("terrainArea").value) || (widths.reduce((a, b) => a + b, 0) * lengths.reduce((a, b) => a + b, 0));
+    const tileArea = parseFloat(document.getElementById("tileArea").value) || tileSizeArea;
 
-    document.getElementById("result").innerText += "\n\nNecesitas comprar " + palmetas + " palmetas de pasto.";
+    const palmetas = Math.ceil(terrainArea / tileArea);
+
+    document.getElementById("result").innerText = "Necesitas comprar " + palmetas + " palmetas de pasto.";
 }
-
